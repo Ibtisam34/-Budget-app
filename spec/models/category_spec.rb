cd97e0bfeebe72a452821383064c33a16b9370c5
unknown <Riyanagwl@gmail.com>
2023-04-07 11:11:49 +0300

    modified:   spec/models/category_spec.rb

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,30 +1,20 @@
 require 'rails_helper'
-
 RSpec.describe Category, type: :model do
+  let(:user) { User.create(name: 'Riyana', email: 'riyanagwl@test.come', password: 'Queen') }
   subject do
-    user = User.create(name: 'Riyana', email: 'riyanagwl@test.come', password: 'Queen')
-    Category.new(author: user, name: 'gym', icon: 'new_icon')
+    Category.new(author: user, name: 'gym', icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test_image.png'), 'image/png'))
   end
-
   before { subject.save }
-
-  it 'subject should be valid' do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
-
   describe 'validations' do
-    it 'not valid without name' do
-      subject.name = ''
+    it 'is not valid without a name' do
+      subject.name = nil
       expect(subject).to_not be_valid
     end
-
-    it 'name exists' do
-      subject.name = 'Lynette'
-      expect(subject).to be_valid
-    end
-
-    it 'invalid with empty icon name' do
-      subject.icon = ''
+    it 'is not valid without an icon' do
+      subject.icon = nil
       expect(subject).to_not be_valid
     end
   end
